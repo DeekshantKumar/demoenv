b4cfb4197612fc710472846576175ba16f8dc8de
DeekshantKumar <117037263+DeekshantKumar@users.noreply.github.com>
2022-12-30 13:22:25 +0530

    modified files for categoy

--- category/migrations/0001_initial.py
+++ category/migrations/0001_initial.py
@@ -9,11 +9,11 @@ class Migration (migrations.Migration) :
         migrations.CreateModel(
             name= 'Category',
             fields= [
-                ('id' , models.AutoField(auto_created = True, primary_key =True, Serialize = False, verbose_name ='ID' ) ),
+                ('id' , models.AutoField(auto_created = True, primary_key =True, serialize = False, verbose_name ='ID' ) ),
                 ('category_name', models.CharField(max_length=50, unique = True)),
                 ('slug' , models.CharField (max_length=100, unique=True)),
                 ('description' ,  models.TextField(max_length=255, blank= True))
-                ('cat_image' , models.ImageField (upload_to= 'photos/category/', blank =True) )
+                ('cat_image' , models.ImageField (blank =True, upload_to= 'photos/category/') )
 
 
             ],
