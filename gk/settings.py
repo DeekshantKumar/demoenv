37ffab88dd6445a301cc43ab6056ceb7ea38733b
DeekshantKumar <117037263+DeekshantKumar@users.noreply.github.com>
2022-12-30 19:19:45 +0530

    added /media/

--- gk/settings.py
+++ gk/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS= ['gk/static',]
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT= BASE_DIR/'media'
 
 # Default primary key field type
