e7b6f7bdfc09287d2d3542b97f1342363d3932f4
DeekshantKumar <117037263+DeekshantKumar@users.noreply.github.com>
2022-12-30 18:59:49 +0530

    added accountas

--- gk/settings.py
+++ gk/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
+    'accounts',
 
 ]
 
@@ -70,6 +71,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'gk.wsgi.application'
+
+AUTH_USER_MODEL = 'accounts.Account'
 
 
 # Database
