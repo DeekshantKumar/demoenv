8f48144611e6ab895396d499ea7a744b39605989
DeekshantKumar <117037263+DeekshantKumar@users.noreply.github.com>
2023-01-04 09:58:34 +0530

    s

--- map/views.py
+++ map/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def indexView (request):
+    return HttpResponse ("<h1> welcome  </h1>")
 
 # Create your views here.
