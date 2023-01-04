26016e1730f14432b74a49f4a19c0fef50b0413a
DeekshantKumar <117037263+DeekshantKumar@users.noreply.github.com>
2023-01-04 09:20:20 +0530

    deleted gee

--- gee/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GeeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gee'
--- gee/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def geehome (request):
-    # return render(request, 'index.html')
-    return HttpResponse ("<h1> hi </h1>")
