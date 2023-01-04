8ff86c9772ff33a75536503c2f9888ded9a12aab
DeekshantKumar <117037263+DeekshantKumar@users.noreply.github.com>
2023-01-04 09:39:25 +0530

    path

--- gk/urls.py
+++ gk/urls.py
@@ -22,6 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('',views.home , name = 'home'),
-    path('map/', include ('map.urls')),
+    path('', include ('map.urls')),
 
 ] + static ( settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- map/urls.py
+++ map/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from . import views
+
 from django.urls import path
+from . import views
 
 urlpatterns = [
 
