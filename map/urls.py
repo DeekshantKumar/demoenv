4d06f12884ba6be6f9cf645dc8c9a73ac069f401
DeekshantKumar <117037263+DeekshantKumar@users.noreply.github.com>
2023-01-05 07:57:12 +0530

    added map views settings

--- map/urls.py
+++ map/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
-from django.urls import path
+
 from . import views
+from .views import *
+from django.urls import path, include
+from google import views as view
+
+
+# urlpatterns = [
+
+#     path ('', views.indexView)
+
+# ]
 
 urlpatterns = [
-
-    path ('', views.indexView)
-
+   
+   path('map',view.map, name="map"),
+   
 ]
--- map/views.py
+++ map/views.py
@@ -3,7 +3,15 @@ from django.http import HttpResponse
 
 
 
-def indexView (request):
-    return HttpResponse ("<h1> welcome  </h1>")
+# def indexView (request):
+#     return HttpResponse ("<h1> welcome  </h1>")
 
 # Create your views here.
+from django.conf import settings
+
+def map(request):
+    key = settings.GOOGLE_API_KEY
+    context = {
+        'key':key,
+    }
+    return render(request, 'google/map.html',context)
